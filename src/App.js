910c7f5530ff4527f39576a42639c12bbfa798ec
Mamta Sharma <123922178+Mamtasharma071@users.noreply.github.com>
2024-10-13 16:08:21 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,9 +2,26 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
-  return <Profile/>;
+  return(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/register" element={<Register />} />
+      <Route path="/profile/:use" element={<Profile/>} />
+
+    </Routes>
+  </Router>
+   ) ;
 }
 
 export default App;
